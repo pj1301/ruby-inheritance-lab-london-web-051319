c6de582de88487fcc40a42a8b6335cad99eacdc7
github username <github email address>
2019-05-17 11:50:52 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require "pry"
+
+class Student  < User # This indicates inheritance from another class
+
+   attr_accessor :knowledge 
+
+   def initialize
+      # binding.pry
+      @knowledge = []
+   end
+
+   def learn(string)
+      @knowledge << string
+   end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+require "pry"
+
+class User  
+   
+   attr_accessor :first_name, :last_name
+
+   @@all = []
+
+   def initialize
+      @@all << self
+   end
+
+   def self.all
+      @@all
+   end
+
+end
